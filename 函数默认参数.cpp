74d00907b11f31afd49cb4537e2b872141a326d2
diwei00 <96779332+diwei00@users.noreply.github.com>
2022-05-17 22:35:09 +0800

    Add files via upload

---
+++ 函数默认参数.cpp
@@ -0,0 +1,26 @@
+//#include <iostream>
+//using namespace std;
+////如果函数声明有参数，函数实现就不能有参数
+////两者只能有一个有参数
+//int fun(int a, int b = 20);
+//
+//int fun(int a, int b)
+//{
+//	return a + b;
+//}
+////函数有默认参数，但传什么值用什么值
+//int fun2(int a, int b = 30)
+//{
+//	return a + b;
+//}
+////如果一个地方有默认参数，则后面参数都得有默认参数
+//int main()
+//{
+//
+//	int tmp = fun(10);
+//	cout << tmp << endl;
+//	fun2(10, 20);
+//
+//	system("pause");
+//	return 0;
+//}
