15e7b0e5984ae6f67901e11f5d56ec7d6263a695
diwei00 <2945608334@qq.com>
2022-05-21 22:00:37 +0800

    初始胡列表

---
+++ 初始化列表.cpp
@@ -0,0 +1,24 @@
+//#include <iostream>
+//using namespace std;
+//class person
+//{
+//public:
+//
+//	
+//	person(int a, int b) :m_A(a), m_B(b)
+//	{
+//
+//	}
+//
+//	int m_A;
+//	int m_B;
+//
+//};
+//int main()
+//{
+//
+//	person p(10,20);
+//	cout << p.m_A << "\t" << p.m_B << endl;
+//	system("pause");
+//	return 0;
+//}
