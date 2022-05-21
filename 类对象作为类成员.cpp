7dee7ccd8e615c93c6ef1ea14e8d89c339f3d5fc
diwei00 <2945608334@qq.com>
2022-05-21 22:01:29 +0800

    类对象作为类成员

---
+++ 类对象作为类成员.cpp
@@ -0,0 +1,34 @@
+//#include <iostream>
+//using namespace std;
+//class phone
+//{
+//public:
+//	phone(string name)
+//	{
+//		pName = name;
+//	}
+//
+//	string pName;
+//
+//};
+//class person
+//{
+//public:
+//
+//	person(string name, string pname):m_name(name), m_PName(pname)
+//	{
+//
+//	}
+//
+//	string m_name;
+//	//构造函数，先调类内成员对象构造函数，再调自己的
+//	//析构函数，顺序相反
+//	phone m_PName;
+//};
+//int main()
+//{
+//	person p("wuhao", "aaaa");
+//	cout << p.m_name << "\t" << p.m_PName .pName<< endl;
+//	system("pause");
+//	return 0;
+//}
